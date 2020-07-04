8c364109a407c8d7c967d55930c71a19c1807515
Samuel F. Ademola <exodus@Samuels-MacBook-Pro-2.local>
2020-07-04 02:51:27 -0500

    3rd commit - added templates, and url_for

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Samuel Ademola',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2019'
+    },
+     {
+        'author': 'Jordan Dockery',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 20, 2020'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello, Sam!</h1>'
+    return render_template('home.htm', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About!</h1>'
+    return render_template('about.htm', title='About')
 
 
 if __name__ == "__main__":
